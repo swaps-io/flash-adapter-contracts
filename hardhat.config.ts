b43d9d01c152586cdb6c8ad4b0aeedcbf5126ef4
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2024-10-10 18:28:06 +0400

    Init project

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import 'hardhat-contract-sizer';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.27',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 1_000_000,
+        details: {
+          yulDetails: {
+            optimizerSteps: 'u',
+          },
+        },
+      },
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: true,
+    runOnCompile: true,
+    strict: true,
+    only: [],
+  },
+};
+
+export default config;
