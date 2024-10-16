40892c4ecd27eb76c54d38e51913adef8196b619
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2024-10-16 19:58:10 +0400

    Add resolver order hash test

--- contracts/test/DynamicResolverTest.sol
+++ contracts/test/DynamicResolverTest.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.26;
 
 import {DelegateDynamicReceiveAdapter, IOrderReceiver, Order} from "../adapter/delegate-dynamic-receive/DelegateDynamicReceiveAdapter.sol";
 
+import {OrderHashLib} from "../flash/order/OrderHashLib.sol";
+
 contract DynamicResolverTest is DelegateDynamicReceiveAdapter {
     constructor()
         DelegateDynamicReceiveAdapter(IOrderReceiver(address(0)))
     {}
+
+    function calcResolverOrderHash(bytes32 resolver_, bytes memory resolverData_) external pure returns (bytes32) {
+        Order memory order = _resolverOrder(resolver_, resolverData_);
+        return OrderHashLib.calcOrderHash(order);
+    }
 
     function resolverOrder(bytes32 resolver_, bytes memory resolverData_) external pure returns (Order memory order) {
         return _resolverOrder(resolver_, resolverData_);
--- test/adapter/delegate-dynamic-receive/DynamicResolverTest.ts
+++ test/adapter/delegate-dynamic-receive/DynamicResolverTest.ts
@@ -100,6 +100,7 @@ describe('DynamicResolverTest', function () {
     } as const;
     const orderOffset = 4n;
 
+    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
     const resolverData = encodeFunctionData({
       abi: resolver.abi,
       functionName: 'receiveDynamicOrderOffset4',
@@ -109,8 +110,6 @@ describe('DynamicResolverTest', function () {
         zeroHash,
       ],
     });
-
-    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
 
     const resolverOrder = await test.read.resolverOrder([dynamicResolver, resolverData]);
     const keys = Object.keys(order) as (keyof typeof order)[];
@@ -144,6 +143,7 @@ describe('DynamicResolverTest', function () {
     } as const;
     const orderOffset = 36n;
 
+    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
     const resolverData = encodeFunctionData({
       abi: resolver.abi,
       functionName: 'receiveDynamicOrderOffset36',
@@ -154,8 +154,6 @@ describe('DynamicResolverTest', function () {
         zeroHash,
       ],
     });
-
-    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
 
     const resolverOrder = await test.read.resolverOrder([dynamicResolver, resolverData]);
     const keys = Object.keys(order) as (keyof typeof order)[];
@@ -189,6 +187,7 @@ describe('DynamicResolverTest', function () {
     } as const;
     const orderOffset = 100n;
 
+    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
     const resolverData = encodeFunctionData({
       abi: resolver.abi,
       functionName: 'receiveDynamicOrderOffset100',
@@ -202,12 +201,51 @@ describe('DynamicResolverTest', function () {
       ],
     });
 
-    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
-
     const resolverOrder = await test.read.resolverOrder([dynamicResolver, resolverData]);
     const keys = Object.keys(order) as (keyof typeof order)[];
     for (const key of keys) {
       expect(resolverOrder[key]).equal(order[key]);
     }
+  });
+
+  it('Should calc resolver order hash', async function () {
+    const { test, resolver } = await loadFixture(deployFixture);
+
+    const order = {
+      fromActor: '0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef',
+      fromActorReceiver: '0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef',
+      fromChain: 512n,
+      fromToken: '0x0101010101010101010101010101010101010101',
+      fromAmount: 1234n,
+      toActor: '0xdeadc0dedeadc0dedeadc0dedeadc0dedeadc0de',
+      toChain: 1024n,
+      toToken: '0x0202020202020202020202020202020202020202',
+      toAmount: 3456n,
+      collateralReceiver: '0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef',
+      collateralChain: 69n,
+      collateralAmount: 1234n,
+      collateralRewardable: 322n,
+      collateralUnlocked: 6996n,
+      deadline: 12345678901234567890n,
+      timeToSend: 112233n,
+      timeToLiqSend: 330101033n,
+      nonce: 421337n,
+    } as const;
+    const orderOffset = 36n;
+
+    const dynamicResolver = encodeDynamicResolver(resolver.address, orderOffset);
+    const resolverData = encodeFunctionData({
+      abi: resolver.abi,
+      functionName: 'receiveDynamicOrderOffset36',
+      args: [
+        zeroHash,
+        order,
+        resolverFlowFlags(),
+        zeroHash,
+      ],
+    });
+
+    const orderHash = await test.read.calcResolverOrderHash([dynamicResolver, resolverData]);
+    expect(orderHash).to.be.equal('0x044807f64a380eac8ee1fff9faca0f89c7015d9461afdd6fd567acc5016d4b7c');
   });
 });
